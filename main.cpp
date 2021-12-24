27e772c23fd40ca93c74be79d9e8ef3565c0b257
bernardo <bernardomny@hotmail.com>
2021-12-23 22:03:34 -0300

    atualizando arquivos

--- main.cpp
+++ main.cpp
@@ -165,21 +165,119 @@ void callQuickSort(int n) {
 
 }
 
+void callCountingSort(int n) {
+
+    FileReader fr;
+
+    int comps[5];
+
+    int moves[5];
+
+    float TotalTimes[5];
+
+    Sorts CountingSort(0);
+
+    vector<TikTokData> cringeData;
+
+    ofstream input("saída.txt", ios::app);
+
+    input << "Counting Sort p/ N = " << n << endl;
+
+    input << "Comparações, Movimentos, Tempo de execução" << endl;
+
+    cringeData = fr.readTikTok("tiktok_app_reviews.csv");
+
+    input.close();
+
+    for(int i = 0; i < 5; i++) {
+
+        srand(i);
+
+        SortData *sortsInfo = new SortData(n, n);
+
+        vector<TikTokData> ol(n);
+
+        for (int j = 0; j < sortsInfo->getKeyVectorSize(); j++) {
+
+            ol[j] = cringeData[sortsInfo->getIndexFromKeyVector(j)];
+
+        }
+
+        sortsInfo->setBeginTimeAction();
+
+        CountingSort.countingSort(ol, sortsInfo);
+        //vector<TikTokData> &data, SortData *info
+
+        sortsInfo->setEndTimeCut();
+
+        sortsInfo->defineTotalTime();
+
+        fr.writeInFile(ol);
+
+        moves[i] = sortsInfo->getMoveCount();
+
+        comps[i] = sortsInfo->getCompCount();
+
+        TotalTimes[i] = sortsInfo->getTotalTime();
+
+        delete sortsInfo;
+
+    }
+
+    float moveMedia = (moves[0] + moves[1] + moves[2] + moves[3] + moves[4])/ 5;
+
+    float compsMedia = (comps[0] + comps[1] + comps[2] + comps[3] + comps[4])/ 5;
+
+    float totalMedia = (TotalTimes[0] + TotalTimes[1] + TotalTimes[2] + TotalTimes[3] + TotalTimes[4])/ 5;
+
+    ofstream results("saída.txt",ios::app);
+
+    results << "Médias: " << fixed << setprecision(3) << moveMedia << ", " << compsMedia << ", " << totalMedia << endl << endl;
+
+    results.close();
+
+}
+
+void callSorts() {
+
+    callHeapSort(10000);
+
+    callHeapSort(50000);
+
+    callHeapSort(100000);
+
+    callHeapSort(500000);
+
+    callHeapSort(1000000);
+
+
+    callQuickSort(10000);
+
+    callQuickSort(50000);
+
+    callQuickSort(100000);
+
+    callQuickSort(500000);
+
+    callQuickSort(1000000);
+
+
+    callCountingSort(10000);
+
+    callCountingSort(50000);
+
+    callCountingSort(100000);
+
+    callCountingSort(500000);
+
+    callCountingSort(1000000);
+
+}
 
 int main()
 {
 
-    callHeapSort(10000);
+    callSorts();
 
-    int n = 50000;
-
-    n = 100000;
-
-    n = 500000;
-
-    n = 1000000;
-
-    //Sorts c(n);
-    std::vector<TikTokData> dados;
     return 0;
 }
